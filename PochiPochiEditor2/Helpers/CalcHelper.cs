ff9f8fc304a478930a3686f1c43090e6c89bdc46
orangenjin <takachizu4@gmail.com>
2026-08-09 10:31:46 +0900

    calc helper add

---
+++ PochiPochiEditor2/Helpers/CalcHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace PochiPochiEditor2.Helpers
+{
+    public static class CalcHelper
+    {
+        /// <summary>
+        /// stringからintへ16進数アドレスを変換する。
+        /// </summary>
+        public static bool TryParseOffset(string offsetStr, out int offsetValue)
+        {
+            return int.TryParse(offsetStr, NumberStyles.HexNumber, null, out offsetValue);
+        }
+    }
+}
