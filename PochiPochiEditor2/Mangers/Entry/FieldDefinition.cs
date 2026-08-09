8828c3f6008fc3d45665790eaca36016b90bee95
orangenjin <takachizu4@gmail.com>
2026-08-09 19:50:36 +0900

    Revert "nest revert"
    This reverts commit 024d2daeb72386507b2bc75195c26bc99d7619a9.

--- PochiPochiEditor2/Mangers/Entry/FieldDefinition.cs
+++ PochiPochiEditor2/Mangers/Entry/FieldDefinition.cs
@@ -5,59 +5,66 @@ namespace PochiPochiEditor2.Mangers.Entry
 {
     public class FieldDefinition
     {
-        public class FieldMetadata
+        public List<FieldMetadata> Fields { get; set; }
+
+        public FieldDefinition()
         {
-            public string Name { get; set; }
-            public FieldKind Kind { get; set; }
-            public List<FieldAttribute> Attributes { get; set; }
+            Fields = new List<FieldMetadata>();
+        }
+    }
 
-            public FieldMetadata(string name, FieldKind kind)
-            {
-                Name = name;
-                Kind = kind;
-                Attributes = new List<FieldAttribute>();
-            }
+    public class FieldMetadata
+    {
+        public string Name { get; set; }
+        public FieldKind Kind { get; set; }
+        public List<FieldAttribute> Attributes { get; set; }
 
-            // Type取得用
-            public Type Type
-            {
-                get
-                {
-                    switch (Kind)
-                    {
-                        case FieldKind.Byte:
-                            return typeof(byte);
-                        case FieldKind.SByte:
-                            return typeof(sbyte);
-                        case FieldKind.UInt16:
-                            return typeof(ushort);
-                        case FieldKind.Int16:
-                            return typeof(short);
-                        case FieldKind.UInt32:
-                            return typeof(uint);
-                        case FieldKind.Int32:
-                            return typeof(int);
-                        default:
-                            return typeof(string); // FieldKind.String
-                    }
-                }
-            }
-
-            // 符号ありかどうかを判定
-            public bool IsSigned =>
-                Kind is FieldKind.SByte || Kind is FieldKind.Int16 || Kind is FieldKind.Int32;
+        public FieldMetadata(string name, FieldKind kind)
+        {
+            Name = name;
+            Kind = kind;
+            Attributes = new List<FieldAttribute>();
         }
 
-        public class FieldAttribute
+        // Type取得用
+        public Type Type
         {
-            public AttributeType AttributeType { get; }
-            public object[] Parameters { get; }
-
-            public FieldAttribute(AttributeType attributeType, params object[] parameters)
+            get
             {
-                AttributeType = attributeType;
-                Parameters = parameters;
+                switch (Kind)
+                {
+                    case FieldKind.Byte:
+                        return typeof(byte);
+                    case FieldKind.SByte:
+                        return typeof(sbyte);
+                    case FieldKind.UInt16:
+                        return typeof(ushort);
+                    case FieldKind.Int16:
+                        return typeof(short);
+                    case FieldKind.UInt32:
+                        return typeof(uint);
+                    case FieldKind.Int32:
+                        return typeof(int);
+                    default:
+                        return typeof(string); // FieldKind.String
+                }
             }
+        }
+
+        // 符号ありかどうかを判定
+        public bool IsSigned =>
+            Kind is FieldKind.SByte || Kind is FieldKind.Int16 || Kind is FieldKind.Int32;
+    }
+
+    public class FieldAttribute
+    {
+        public AttributeType AttributeType { get; }
+        public object[] Parameters { get; }
+
+        public FieldAttribute(AttributeType attributeType, params object[] parameters)
+        {
+            AttributeType = attributeType;
+            Parameters = parameters;
         }
     }
 
