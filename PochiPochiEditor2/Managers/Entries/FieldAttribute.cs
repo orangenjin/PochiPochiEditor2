651dffce4690d050cda320571af3b386832e0cfe
orangenjin <takachizu4@gmail.com>
2026-08-09 19:58:35 +0900

    field def re

---
+++ PochiPochiEditor2/Managers/Entries/FieldAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace PochiPochiEditor2.Managers.Entries
+{
+    public class FieldAttribute
+    {
+        public AttributeType AttributeType { get; }
+        public object[] Parameters { get; }
+
+        public FieldAttribute(AttributeType attributeType, params object[] parameters)
+        {
+            AttributeType = attributeType;
+            Parameters = parameters;
+        }
+    }
+
+    public enum AttributeType
+    {
+        StringAttribute,
+        NibbleAttribute,
+        BitAttribute
+    }
+}
