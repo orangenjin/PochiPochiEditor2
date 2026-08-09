f800c347ab6a7827b46e4e3e4adbba0ff89cc0ed
orangenjin <takachizu4@gmail.com>
2026-08-09 11:16:51 +0900

    constants

---
+++ PochiPochiEditor2/Constants.cs
@@ -0,0 +1,48 @@
+﻿namespace PochiPochiEditor2
+{
+    public static class Constants
+    {
+        public const int HexBase = 16;
+        public const int BitsPerByte = 8;
+        public const int CharPerByte = 2;
+        public const int NibbleShift = 4;
+        public const int NibbleMask = 0xF;
+        public const int ByteMask = 0xFF;
+        public const int UShortMask = 0xFFFF;
+        public const uint UIntMask = 0xFFFFFFFFU;
+        public const int ByteSize = 1;
+        public const int UShortSize = 2;
+        public const int UIntSize = 4;
+        public const uint BaseAddr = 0x8000000U;
+        public const int InvalidOffset = -1;
+        public const string InvalidString = "null";
+        public const string HexPrefix = "0x";
+
+        public const byte PaddingByte = 0x0;
+        public const byte FreeSpaceByte = 0xFF;
+        public const byte StrNewlineByte = 0xFE;
+        public const byte StrTerminatorByte = 0xFF;
+
+        public const int PalColorCount = 16;
+        public const int BytesPerColor = 2;
+        public const int ColorChannelMulti = 8;
+        public const int RedShift = 0;
+        public const int GreenShift = 5;
+        public const int BlueShift = 10;
+        public const int RedMask = 0x1F;
+        public const int GreenMask = 0x3E0;
+        public const int BlueMask = 0x7C00;
+        public const int ArgbByteCount = 4;
+
+        public const int TileSize = 8;
+        public const int Bpp4 = 4;
+        public const int PixelsPerByte4Bpp = BitsPerByte / Bpp4;
+        public const int SpriteSize = 64;
+        public const int DefaultScale = 2;
+
+        public const string RomFileFilter = "ROMファイル|*.gba";
+        public const string ImageImportFilter = "画像ファイル (*.png;*.bmp)|*.png;*.bmp";
+        public const string ImageExportFilter = "PNG画像 (*.png)|*.png|BMP画像 (*.bmp)|*.bmp";
+        public const string BinImportExportFilter = "BINファイル (*.bin)|*.bin";
+    }
+}
