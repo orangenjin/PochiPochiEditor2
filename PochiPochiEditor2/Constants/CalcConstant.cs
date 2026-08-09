eeb194fb82f0974da0e9bf21087d4a814d371cc2
orangenjin <takachizu4@gmail.com>
2026-08-09 11:10:35 +0900

    calc constant

---
+++ PochiPochiEditor2/Constants/CalcConstant.cs
@@ -0,0 +1,22 @@
+﻿namespace PochiPochiEditor2.Constants
+{
+    public static class CalcConstant
+    {
+        public const int HexBase = 16;
+        public const int BitsPerByte = 8;
+        public const int CharPerByte = 2;
+        public const int NibbleShift = 4;
+        public const int NibbleMask = 0xF;
+        public const int ByteMask = 0xFF;
+        public const int UShortMask = 0xFFFF;
+        public const uint UIntMask = 0xFFFFFFFFU;
+        public const int ByteSize = 1;
+        public const int UShortSize = 2;
+        public const int UIntSize = 4;
+        public const uint BaseAddr = 0x8000000U;
+
+        public const int InvalidOffset = -1;
+        public const string InvalidString = "null";
+        public const string HexPrefix = "0x";
+    }
+}
