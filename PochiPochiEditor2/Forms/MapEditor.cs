5f15f57ae3094b226ecabe6f5fd7362d398e5ae2
orangenjin <takachizu4@gmail.com>
2026-08-27 22:16:12 +0900

    map name load

--- PochiPochiEditor2/Forms/MapEditor.cs
+++ PochiPochiEditor2/Forms/MapEditor.cs
@@ -26,7 +26,20 @@ namespace PochiPochiEditor2.Forms
         // 変更履歴用
         private UndoManager _undoManager = null;
         // 各テーブル用
-        private EntryManager _headerEntry = null;
+        private EntryManager _mapNameEntry = null;
+
+        private enum FieldKey
+        {
+            MapNamePointerOffset,
+        }
+
+        private static class IniKey
+        {
+            public static string MapNamePointerEntry = nameof(MapNamePointerEntry);
+            public static string MapNameTableOffset = nameof(MapNameTableOffset);
+            public static string MapNameCount = nameof(MapNameCount);
+            public static string MapNameFirstIndex = nameof(MapNameFirstIndex);
+        }
 
         public MapEditor(SharedData sharedData, UndoManager undoManager)
         {
@@ -44,11 +57,17 @@ namespace PochiPochiEditor2.Forms
 
         private void InitializeEntries()
         {
+            // マップ名テーブルを作成
+            string defFileName = IniKey.MapNamePointerEntry;
+            int tableOffset = _sharedData.Config.ReadInt(IniKey.MapNameTableOffset);
+            int entrycount = _sharedData.Config.ReadInt(IniKey.MapNameCount);
+            _mapNameEntry = new EntryManager(defFileName, typeof(FieldKey), _sharedData, tableOffset, entrycount);
 
         }
 
         private void InitializeControls()
         {
+            // 各コンボボックスにアイテムを追加
             CtrlHelper.LoadComboBoxFromFile(
                 (cmbMapType, "txt/Map/MapType.txt"),
                 (cmbMapWthr, "txt/Map/MapWthr.txt"),
@@ -56,6 +75,35 @@ namespace PochiPochiEditor2.Forms
                 (cmbMapBike, "txt/Map/MapBike.txt"),
                 (cmbMapSpBg, "txt/Map/MapSpBg.txt"),
                 (cmbMapNameType, "txt/Map/MapNameType.txt"));
+            UpdateMapNameComboBox();
+        }
+
+        private void UpdateMapNameComboBox()
+        {
+            cmbMapNameIndex.BeginUpdate();
+            cmbMapNameIndex.Items.Clear();
+
+            // 基準となる最初のインデックスを設定
+            int firstIndex = _sharedData.Config.ReadInt(IniKey.MapNameFirstIndex);
+
+            // 順次格納していく
+            for (int i = 0; i < _mapNameEntry.Entries.Count; i++)
+            {
+                var offset =
+                    _mapNameEntry.Entries[i][FieldKey.MapNamePointerOffset].GetData<int>();
+                var mapName =
+                    _sharedData.Charmap.BytesToString(_sharedData.RomData, offset);
+
+                cmbMapNameIndex.Items.Add($"[{firstIndex + i:X2}]{mapName}");
+            }
+
+            cmbMapNameIndex.EndUpdate();
+
+            // 初期選択
+            if (cmbMapNameIndex.Items.Count > 0)
+            {
+                cmbMapNameIndex.SelectedIndex = 0;
+            }
         }
     }
 }
